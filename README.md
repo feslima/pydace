ba5268bb9261bbb56ddd28cb0afe4a925d9b86a0
Felipe Lima <felipe.lima@eq.ufcg.edu.br>
2019-03-29 16:33:20 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Description
+
+This project is an adaptation from the work of Hans Bruun Nielsen, Søren Nymand and Lophaven Jacob Søndergaard.
+
+## Installation
+
+To be implemented.
+
+## Usage
+
+To be implemented.
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='pydace',
+      version='0.0.1',
+      description='Desing and Analysis of Computational Experiments toolbox.',
+      author='Felipe Souza Lima',
+      author_email='felipe.lima@eq.ufcg.edu.br',
+      license='Apache License 2.0',
+      packages=find_packages(exclude=['tests']),
+      long_description=long_description,
+      keywords="surrogate, metamodel",
+      install_requires=['numpy', 'scipy']
+      )
