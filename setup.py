53fad90cf479b68ac64f72add96487a407bb3989
Felipe <felipe.lima@eq.ufcg.edu.br>
2019-04-02 09:47:26 -0300

    version update, github repo inclusion and classifiers.

--- setup.py
+++ setup.py
@@ -4,14 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pydace',
-      version='0.0.1',
-      description='Desing and Analysis of Computational Experiments toolbox.',
+      version='0.0.2',
+      description='Desing and Analysis of Computational Experiments as python toolbox.',
       author='Felipe Souza Lima',
       author_email='felipe.lima@eq.ufcg.edu.br',
+      url='https://github.com/feslima/pydace',
       license='Apache License 2.0',
       packages=find_packages(exclude=['tests']),
       long_description=long_description,
       keywords="surrogate, metamodel",
       setup_requires=['numpy'],
-      install_requires=['scipy']
+      install_requires=['scipy'],
+      classifiers=[
+          "Programming Language :: Python :: 3.6",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+          "Topic :: Scientific/Engineering",
+      ]
       )
