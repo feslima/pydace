b933dbb02c4bc6f5e846fae399a537923c59fccb
Felipe <felipe.lima@eq.ufcg.edu.br>
2019-07-25 14:19:52 -0300

    version bump to 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pydace',
-      version='0.0.4',
+      version='0.1.0',
       description='Design and Analysis of Computational Experiments as python toolbox.',
       author='Felipe Souza Lima',
       author_email='felipe.lima@eq.ufcg.edu.br',
@@ -12,9 +12,10 @@ setup(name='pydace',
       license='Apache License 2.0',
       packages=find_packages(exclude=['tests']),
       long_description=long_description,
+      long_description_content_type="text/markdown",
       keywords="surrogate, metamodel",
-      setup_requires=['numpy>=1.15'],
-      install_requires=['scipy>=1.2.0', 'pyDOE2'],
+      setup_requires=['numpy>=1.16'],
+      install_requires=['scipy>=1.3.0', 'pyDOE2'],
       python_requires='>=3.5',
       classifiers=[
           "Programming Language :: Python :: 3.6",
