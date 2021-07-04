b38e7fc2ef2945caaa67ca3653888d2deebb76fd
Felipe <feslima93@gmail.com>
2021-07-04 11:03:23 -0300

    Bumping version and updating requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 PACKAGE_NAME = 'pydace'
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 
 setup(name=PACKAGE_NAME,
       version=VERSION,
@@ -18,8 +18,12 @@ setup(name=PACKAGE_NAME,
       long_description=long_description,
       long_description_content_type="text/markdown",
       keywords="surrogate, metamodel",
-      install_requires=['numpy>=1.16', 'scipy>=1.2.0', 'pyDOE2'],
-      python_requires='>=3.5',
+      install_requires=[
+          'numpy>=1.16', 
+          'scipy>=1.2.0', 
+          'pyDOE2>=1.2.1'
+          ],
+      python_requires='>=3.7',
       classifiers=[
           "Programming Language :: Python :: 3.6",
           "License :: OSI Approved :: Apache Software License",
